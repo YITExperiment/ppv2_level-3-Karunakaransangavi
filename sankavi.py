2fc3f8a0c7fa060be92aca6621b8299602b1f947
Karunakaransangavi <137860212+Karunakaransangavi@users.noreply.github.com>
2023-06-29 14:19:00 +0530

    Add files via upload

---
+++ sankavi.py
@@ -0,0 +1,13 @@
+Python 3.11.4 (tags/v3.11.4:d2340ef, Jun  7 2023, 05:45:37) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+================================= RESTART: C:/Users/MARS PC/AppData/Local/Programs/Python/Python311/sankavi/sankavi.py =================================
+hello,world!
+>>> print('hello,world!')
+hello,world!
+>>> person=input('what is your name?')
+what is your name?
+================================= RESTART: C:/Users/MARS PC/AppData/Local/Programs/Python/Python311/sankavi/sankavi.py =================================
+hello,world!
+what is your name? sankavi
+hello  sankavi
